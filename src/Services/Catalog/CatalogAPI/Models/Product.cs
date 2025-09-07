cea1f7289827ca3533a65f105cc032cb8944b909
Arun Joshi <43997581+arunnjoshi@users.noreply.github.com>
2025-09-07 22:34:29 +0530

    #comment adding files.

---
+++ src/Services/Catalog/CatalogAPI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace CatalogAPI.Models;
+
+public class Product
+{
+	public Guid Id { get; set; }
+	public string Name { get; set; }  = null!;
+	public string Description { get; set; }  = null!;
+	public string ImageFile { get; set; }  = null!;
+	public decimal Price { get; set; }  = default;
+	public List<string> Categories { get; set; }  = [];
+}
