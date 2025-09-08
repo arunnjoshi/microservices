93765fa32c9ab65a1d57784ec0e581452fca6c47
Arun Joshi <43997581+arunnjoshi@users.noreply.github.com>
2025-09-08 23:31:40 +0530

    #comment adding carter and mapster.

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,34 @@
-﻿namespace CatalogAPI.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+
+namespace CatalogAPI.Products.CreateProduct;
+
+public record CreateProductCommand
+(
+	string Name,
+	List<string> Category,
+	string Description,
+	string ImageFile,
+	decimal Price
+) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-	public class CreateProductHandler
+	public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
 	{
+		//create product entity from command object
+		//Save to DB
+		// return
+		var product = new Product
+		{
+			Name = command.Name,
+			Categories = command.Category,
+			Description = command.Description,
+			ImageFile = command.ImageFile,
+			Price = command.Price,
+		};
+		return new CreateProductResult(Guid.NewGuid());
 	}
 }
--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,8 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the DI container
-
+builder.Services.AddCarter();
+builder.Services.AddMediatR(c =>
+{
+	c.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 var app = builder.Build();
 //Configure the http request pipeline
+app.MapCarter();
 
 app.Run();
