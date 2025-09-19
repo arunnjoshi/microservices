2e39213e2855160446a81583e3e95c85aa3def6c
Arun Joshi <43997581+arunnjoshi@users.noreply.github.com>
2025-09-19 22:45:36 +0530

    #comment basket repo.

--- src/Services/Catalog/Basket/Basket.API/Program.cs
+++ src/Services/Catalog/Basket/Basket.API/Program.cs
@@ -2,6 +2,7 @@ using BuildingBlocks.Behavior;
 var programAssembly = typeof(Program).Assembly;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Database");
 //Add Services to DI container
 builder.Services.AddCarter();
 builder.Services.AddMediatR(c =>
@@ -10,7 +11,15 @@ builder.Services.AddMediatR(c =>
 	c.AddOpenBehavior(typeof(ValidationBehavior<,>));
 	c.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
 builder.Services.AddValidatorsFromAssembly(programAssembly);
+
+builder.Services.AddMarten(o =>
+{
+	o.Connection(connectionString!);
+	o.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 //Configure the HTTP request pipeline
