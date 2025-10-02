14a81cf31fba5e44479b4c9178666d50ee7c31ee
Arun Joshi <43997581+arunnjoshi@users.noreply.github.com>
2025-10-02 22:31:05 +0530

    #comment config for order  & order iteam

--- Ordering.Infrastructure/Data/Configurations/OrderConfigurations.cs
+++ Ordering.Infrastructure/Data/Configurations/OrderConfigurations.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Enums;
 using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Infrastructure.Data.Configurations;
 
@@ -8,6 +10,121 @@ public class OrderConfigurations : IEntityTypeConfiguration<Order>
 {
 	public void Configure(EntityTypeBuilder<Order> builder)
 	{
-		
+		builder.HasKey(x => x.Id);
+
+		builder.Property(x => x.Id)
+				.HasConversion
+				(
+					orderId => orderId.Value,
+					dbId => OrderId.Of(dbId)
+				);
+
+		builder.HasOne<Customer>()
+				.WithMany()
+				.HasForeignKey(x => x.CustomerId);
+
+		builder.HasMany(x => x.orderItems).WithOne()
+				.HasForeignKey(x => x.OrderId);
+
+		builder.ComplexProperty(x => x.OrderName, nameBuilder =>
+		{
+			nameBuilder.Property(n => n.Value)
+						.HasColumnName(nameof(Order.OrderName))
+						.HasMaxLength(100)
+						.IsRequired();
+		});
+
+		builder.ComplexProperty(x => x.ShippingAddress, addressBuilder =>
+		{
+			addressBuilder.Property(n => n.FirstName)
+						.HasMaxLength(50)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.LastName)
+						.HasMaxLength(50)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.EmailAddress)
+						.HasMaxLength(50)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.AddressLine)
+						.HasMaxLength(180)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.Country)
+						.HasMaxLength(50)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.State)
+						.HasMaxLength(100)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.ZipCode)
+						.HasMaxLength( 5)
+						.IsRequired();
+		});
+
+		builder.ComplexProperty(x => x.BillingAddress, addressBuilder =>
+		{
+			addressBuilder.Property(n => n.FirstName)
+						.HasMaxLength(50)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.LastName)
+						.HasMaxLength(50)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.EmailAddress)
+						.HasMaxLength(50)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.AddressLine)
+						.HasMaxLength(180)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.Country)
+						.HasMaxLength(50)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.State)
+						.HasMaxLength(100)
+						.IsRequired();
+
+			addressBuilder.Property(n => n.ZipCode)
+						.HasMaxLength(5)
+						.IsRequired();
+		});
+
+		builder.ComplexProperty(x => x.Payment, paymentBuilder =>
+		{
+			paymentBuilder.Property(n => n.CardNumber)
+						.HasMaxLength(24)
+						.IsRequired();
+
+			paymentBuilder.Property(n => n.CardName)
+						.HasMaxLength(50)
+						.IsRequired();
+
+			paymentBuilder.Property(n => n.ExpirationDate)
+						.HasMaxLength(10)
+						.IsRequired();
+
+			paymentBuilder.Property(n => n.CVV)
+						.HasMaxLength(180)
+						.IsRequired();
+
+			paymentBuilder.Property(n => n.PaymentMethod)
+						.IsRequired();
+		});
+
+		builder.Property(x=>x.Status)
+			.HasDefaultValue(OrderStatus.Draft)
+			.HasConversion(
+				status => status.ToString(),
+				dbStatus => (OrderStatus)Enum.Parse(typeof(OrderStatus), dbStatus))
+			.IsRequired();
+
+		builder.Property(c => c.TotalPrice);
 	}
 }
--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -5,14 +5,14 @@ public class OrderItem : Entity<OrderItemId>
 	internal OrderItem(OrderId orderId, ProductId productId, decimal price, int quantity)
 	{
 		Id = OrderItemId.Of(Guid.NewGuid());
-		OrderId = orderId.Value;
-		ProductId = productId.Value;
+		OrderId = orderId;
+		ProductId = productId;
 		Price = price;
 		Quantity = quantity;
 	}
 
-	public Guid OrderId { get; private set; } = default!;
-	public Guid ProductId { get; private set; } = default!;
+	public OrderId OrderId { get; private set; } = default!;
+	public ProductId ProductId { get; private set; } = default!;
 	public decimal Price { get; private set; } = default!;
 	public int Quantity { get; private set; } = default!;
 }
